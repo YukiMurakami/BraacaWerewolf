ffe15b7491cf6e166ab7bd03f0b1572681885264
YukiMurakami <muru31415926@gmail.com>
2016-10-21 18:56:58 +0900

    add server and client scene transition

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SceneTransition : MonoBehaviour {
+
+	public Button serverButton;
+	public Button clientButton;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void toServer() {
+		SceneManager.LoadScene ("server");
+	}
+
+	public void toClient() {
+		SceneManager.LoadScene ("client");
+	}
+}
